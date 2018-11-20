547aa08af6ffb7f60e61e1b72212df66963dc2f5
SadLitleMan <43964778+SadLitleMan@users.noreply.github.com>
2018-11-21 00:10:48 +0100

    Update PlayerMov.cs

--- Slider-game/Assets/PlayerMov.cs
+++ Slider-game/Assets/PlayerMov.cs
@@ -20,8 +20,8 @@ public class PlayerMov : MonoBehaviour {
     {
         //rb.AddForce(transform.forward * thrust* Time.deltaTime);
         rb.AddForce(0,0,1 * fowardForce * Time.deltaTime);
-        if (Input.GetKey("space"))
-            rb.AddForce(0, 1 * jump * Time.deltaTime, 0);
+        //if (Input.GetKey("space"))
+          //  rb.AddForce(0, 1 * jump * Time.deltaTime, 0);
         if (Input.GetKey("a"))
             rb.AddForce(-sideForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
         if (Input.GetKey("d"))
